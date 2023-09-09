5703fc6b1217f42f1b304f865f8e8dc13b636c92
Utku Ozer <ozerutku13@gmail.com>
2023-09-09 18:27:22 +0300

    last design

--- form.py
+++ form.py
@@ -1,16 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, PasswordField, EmailField, SubmitField
+from wtforms import StringField, IntegerField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email
 from flask_ckeditor import CKEditorField
 #you need to import this extra.
 
 class SignUpForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
-    email = EmailField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = StringField('Password', validators=[DataRequired()])
     submit = SubmitField('Submit!')
 
 class SignInFormForm(FlaskForm):
-    email = EmailField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = StringField('Password', validators=[DataRequired()])
     submit = SubmitField('Submit!')
